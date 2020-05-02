9610eb9a214c6c47e6d920ff3d48e80c8a895b96
Soni-L <acdsoni@gmail.com>
2020-05-02 19:15:13 +0200

    google sign-in backend only

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -38,6 +40,7 @@ class SignIn extends React.Component {
                     <FormInput name='password' type="password" value={this.state.password} handleChange={this.handleChange} label="password" required />
 
                     <CustomButton type='submit'> Sign in </CustomButton>
+                    <CustomButton onClick={signInWithGoogle}> Sign in with Google</CustomButton>
                 </form>
             </div>
         );
