ca73058dde38f26f5eabd31e1a6d8cb8105ae090
Soni-L <acdsoni@gmail.com>
2020-05-03 19:28:35 +0200

    sign in - sign out in the UI

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -39,8 +39,10 @@ class SignIn extends React.Component {
                     <FormInput name='email' type="email" value={this.state.email} handleChange={this.handleChange} label="email" required />
                     <FormInput name='password' type="password" value={this.state.password} handleChange={this.handleChange} label="password" required />
 
-                    <CustomButton type='submit'> Sign in </CustomButton>
-                    <CustomButton onClick={signInWithGoogle}> Sign in with Google</CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type='submit'> Sign in </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn={true}> Sign in with Google</CustomButton>
+                    </div>
                 </form>
             </div>
         );
